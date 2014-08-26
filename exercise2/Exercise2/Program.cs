cd9be2f3ee6db1b914c353bdda4dd48fe16dc946
Lars Storjord <larsstor@ifi.uio.no>
2014-08-26 22:19:35 +0200

    Exercise 2 base files

---
+++ exercise2/Exercise2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Heavily inspired by https://www.youtube.com/watch?v=6_GTdR0gBVE
+namespace Exercise2 {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Starting");
+
+            var worker = new Worker();
+            worker.DoWork();
+
+            Console.WriteLine("Done");
+        }
+    }
+}
---
+++ exercise2/Exercise2/Worker.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Exercise2 {
+    class Worker {
+        public bool IsComplete { get; private set; }
+
+        public void DoWork()
+        {
+            IsComplete = false;
+            LongOperation();
+            IsComplete = true;
+        }
+
+        private void LongOperation()
+        {
+            Console.WriteLine("Working...");
+            Thread.Sleep(3000);
+        }
+    }
+}
