edb92ad44498f474ad5f886a6e2ad8c3064c4472
Lars Storjord <larsstor@ifi.uio.no>
2014-08-26 22:24:01 +0200

    Add progress tracker

--- exercise2/Exercise2/Program.cs
+++ exercise2/Exercise2/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 // Heavily inspired by https://www.youtube.com/watch?v=6_GTdR0gBVE
@@ -14,6 +15,12 @@ namespace Exercise2 {
             worker.DoWork();
 
             Console.WriteLine("Done");
+        }
+
+        private static void ReportProgress()
+        {
+            Console.Write('.');
+            Thread.Sleep(100);
         }
     }
 }
